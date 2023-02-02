7a3ad82de2422984b4610a30241f2ef9e7004902
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-02-02 14:31:41 +0100

    resume updated

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -20,149 +20,162 @@ const resume = () => {
       <div className="text-center py-4 text-xl font-bold uppercase tracking-wider">
         <div className="hidden sm:block">
           <p>
-            Proven Leadership <span className="px-1">|</span> Web Development{" "}
+            Web Development <span className="px-1">|</span> Backend Development
             <span className="px-1">|</span> Complex Problem Solving
           </p>
         </div>
         <div className="block sm:hidden">
-          <p>Proven Leadership</p>
-          <p className="py-2">Web Development</p>
+          <p>Web Development</p>
+          <p className="py-2">Backend Development</p>
           <p>Complex Problem Solving</p>
         </div>
       </div>
       <p>
-        Analytical, innovative, and motivated web development professional with
-        experience in customer service, team leadership, and organizational
-        effectiveness in fast-paced and challenging environments. Adept at
-        developing strategies and driving streamlined operations. Diverse
-        analytical skills, team collaboration, and relationship building.
-        Consummate professional, and motivated leader, with solid interpersonal
-        abilities and complex problem-solving skills. Effective and proven track
-        record of critical thinking, idea generation, and optimizing
-        efficiencies.
+        Passionate about software development with more than 5 years of
+        experience in developing web applications, backend systems and software
+        test automation. Possesses good communication skills, self-motivation,
+        mathematical thinking; proactive, task-oriented problem solver and team
+        player with good numeracy skills and the ability to quickly learn new
+        technologies and systems. Ability to work independently or in a team to
+        identify and solve problems, plan and complete tasks. Practical
+        experience in agile development environments. Ability to deal with
+        challenging situations under time pressure.
       </p>
 
       {/* Skills */}
       <div className="text-center py-4">
-        <h5 className="text-center underline text-[18px] py-2">Skills</h5>
-        <p className="py-2">
-          <span className="font-bold">Technical Skills</span>
-          <span className="px-2">|</span>Front-End Web Developer
-          <span className="px-2">|</span> HTML
+        <h5 className="text-center underline text-[18px] py-2">
+          Technical Skills
+        </h5>
+        <p className="py-2 text-left">
+          <span className="font-bold">Backend</span>
+          <span className="px-2">:</span>Java Spring Boot
+          <span className="px-2">|</span>Nodejs Express
+        </p>
+        <p className="py-2 text-left">
+          <span className="font-bold">Frontend</span>
+          <span className="px-2">:</span>HTML
           <span className="px-2">|</span>CSS
           <span className="px-2">|</span>Javascript
-          <span className="px-2">|</span>React
-          <span className="px-2">|</span>Next JS
-          <span className="px-2">|</span>SQL
-          <span className="px-2">|</span>NoSQL
+          <span className="px-2">|</span>ReactJS
+          <span className="px-2">|</span>NextJS
           <span className="px-2">|</span>Redux
+          <span className="px-2">|</span>TypeScript
+          <span className="px-2">|</span>MaterialUI
           <span className="px-2">|</span>Tailwind
-          <span className="px-2">|</span> Firebase
-          <span className="px-2">|</span> RESTAPI
+          <span className="px-2">|</span>Bootstrap
+          <span className="px-2">|</span>SASS
         </p>
-        <p className="py-2">
-          <span className="font-bold">Amazon Web Services</span>
-          <span className="px-2">|</span>Amazon Web Services Cloud Practitioner
+        <p className="py-2 text-left">
+          <span className="font-bold">Databases</span>
+          <span className="px-2">:</span>MySQL
+          <span className="px-2">|</span>PostgreSQL
+          <span className="px-2">|</span>MongoDB
+        </p>
+        <p className="py-2 text-left">
+          <span className="font-bold">Test Automation</span>
+          <span className="px-2">:</span>Selenium
+          <span className="px-2">|</span>TestNG
+          <span className="px-2">|</span>Cucumber
+          <span className="px-2">|</span>Rest Assured
+          <span className="px-2">|</span>JUnit
+          <span className="px-2">|</span>Appium
+        </p>
+        <p className="py-2 text-left">
+          <span className="font-bold">Tools</span>
+          <span className="px-2">:</span>Git/Github
+          <span className="px-2">|</span>Jira
+          <span className="px-2">|</span>Xray
+          <span className="px-2">|</span>Confluence
+          <span className="px-2">|</span>Postman
+          <span className="px-2">|</span>Swagger
+          <span className="px-2">|</span>OpenAPI
+          <span className="px-2">|</span>Jenkins
+          <span className="px-2">|</span>Docker
+          <span className="px-2">|</span>Maven
         </p>
       </div>
 
       <h5 className="text-center underline text-[18px] py-4">
         Professional Experience
       </h5>
-      {/* Experience */}
+      {/* Experience 1*/}
       <div className="py-6">
         <p className="italic">
-          <span className="font-bold italic">INFINITE TECH</span>
-          <span className="px-2">|</span>Dallas, TX
+          <span className="font-bold italic">Simpleworks Gmbh</span>
+          <span className="px-2">|</span>Duisburg, DE
         </p>
-        <p className="py-1 italic">
-          Front End Web Developer & Web Master (2013 - Current)
+        <p className="py-1 italic">Performance Engineer (2022 - Current)</p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Successful development of the whole Android test project
         </p>
-        <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
-          <li>
-            Ensured comprehensive mobile responsive UI layouts with
-            understanding of HTML, CSS, and JavaScript.
-          </li>
-          <li>
-            Troubleshoot issues and concerns, configure operation and analytic
-            plugins, and design changes as needed.
-          </li>
-          <li>
-            Worked directly with a diverse client base to understand and
-            implement multiple technologies and programs.
-          </li>
-          <li>
-            Platform migration managing DNS servers as well as MX records and
-            ensuring a smooth migration process.
-          </li>
-        </ul>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Scripted unique test plans, test scripts and processes to remove
+          previously known redundancies by 40% and ensure predictable results
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Development of a Dockerfile to automate the API testing process,
+          increasing efficiency by 65%
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Automated process to create usage charts and increase accuracy
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Development of a Github workflow to automate app distribution
+        </p>
       </div>
-
-      {/* Personal Experience */}
+      {/* Experience 2*/}
       <div className="py-6">
         <p className="italic">
-          <span className="font-bold italic">
-            Teaching & Mentoring Web Development
-          </span>
-          <span className="px-2">|</span>Dallas, TX
+          <span className="font-bold italic">Mersys LLC</span>
+          <span className="px-2">|</span>USA
         </p>
-        <p className="py-1 italic">
-          Code Commerce - Youtube Channel (2021 - Current)
+        <p className="py-1 italic">QA Engineer (01/2021 - 10/2021)</p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Develop and run automated smoke and regression tests and increase test
+          efficiency by 65%
         </p>
-        <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
-          <li>
-            In 2021 I started “Code Commerce”, a YouTube channel focused on
-            sharing what I’m learning in web development, specifically front-end
-            development.
-          </li>
-          <li>
-            Private 1 on 1 Zoom coaching calls related to web development. More
-            specifically debugging, teaching basic web development concepts and
-            code reviews.
-          </li>
-        </ul>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          More than 100 errors found on both server and client side
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Using Selenium WebDriver and Java Designing automation test scenarios
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Development and implementation of Page Object Model based on
+          automation tests
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Used SCRUM and Agile methods and JIRA at work
+        </p>
       </div>
-
-      {/*  */}
-      <h5 className="text-center underline text-[18px] py-4">
-        Other Professional Experience
-      </h5>
-
-      {/* Experience */}
+      {/* Experience 3*/}
       <div className="py-6">
         <p className="italic">
-          <span className="font-bold">CITY OF SHERMAN, TX</span>
-          <span className="px-2">|</span>Sherman, TX
+          <span className="font-bold italic">Turacoon GmbH</span>
+          <span className="px-2">|</span>Stutgart, DE
         </p>
-        <p className="py-1 italic">Paramedic / Firefighter (2010 – 2019)</p>
-        <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
-          <li>
-            Ensured proper implementation of firefighting techniques and
-            practices including controlling and extinguishing fires, operating
-            hose lines, and fire ground operations, ventilation, and search and
-            rescue.
-          </li>
-          <li>
-            Oversaw comprehensive understanding, operation, and maintenance of
-            fire engines, ladder trucks, technical rescue apparatus, and
-            response trailers.
-          </li>
-          <li>
-            Performed ALS techniques as needed within emergency situations
-            including the use of an esophageal or dual lumen airway device and
-            oral intubation using laryngoscopy.
-          </li>
-          <li>
-            Collaborated with a talented and high performing team of
-            firefighters in high pressure and stressful situations as needed.
-          </li>
-          <li>
-            Utilized proven and demonstrated knowledge of multiple
-            certifications including Pediatric Advanced Life Support,
-            Prehospital Trauma Life Support, and Advanced Cardiac Life Support,
-            among others.
-          </li>
-        </ul>
+        <p className="py-1 italic">Software Entwickler (2020 - 2021)</p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Reworked critical modules within a sprint to rely on a centralized
+          library and optimize performance by 60%
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Experience writing modular and reusable code using build tools and
+          package managers
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Development of backend APIs using with MongoDB and NodeJS
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Development of interactive, responsive and scalable frontend and
+          mobile applications using HTML5, CSS3, JavaScript ES6, React, Redux,
+          React Native, Git & Github
+        </p>
+        <p className="px-7 py-1 leading-relaxed list-item list-inside">
+          Participation in Daily Scrum, Sprint Planning, Sprint Review and
+          Sprint Retro meetings
+        </p>
       </div>
     </div>
   );
